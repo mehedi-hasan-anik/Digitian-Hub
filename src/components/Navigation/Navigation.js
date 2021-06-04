2994670bf4427ef20f39fc57b2871df9df49105a
mehedi-hasan-anik <anikm2283@gmail.com>
2021-06-04 15:56:55 +0600

    addReadme

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
         <div className="full-nav-area">
         <nav class="navbar navbar-expand-lg   container">
            <div class="container-fluid nav-full-area">
-               <a class="navbar-brand" href="#">BookArea</a>
+               <a class="navbar-brand" href="#">BreedsHouse</a>
                <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                    <span  class="navbar-toggler-icon">
                       <FontAwesomeIcon icon={faBars} className="nav-bar-icon" />
